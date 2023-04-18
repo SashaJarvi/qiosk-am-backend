53cca8c43ad25e56f0db578a1747bf9ca75c2a0f
SashaJarvi <alexandrermakov2@gmail.com>
2023-04-19 00:12:20 +0400

    added emails sending

--- src/api/event/controllers/event.ts
+++ src/api/event/controllers/event.ts
@@ -32,38 +32,6 @@ export default factories.createCoreController('api::event.event', ({ strapi }) =
       `,
     });
 
-    // const emailTemplate = {
-    //   subject: 'Заявка с сайта qiosk.am',
-    //   text: message,
-    //   html: `
-    //     <table style="width: 100%; border-collapse: collapse">
-    //       <tr>
-    //         <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Name</b></td>
-    //         <td style='padding: 10px; border: #e9e9e9 1px solid;'>${name}</td>
-    //       </tr>
-    //       <tr style="background-color: #f8f8f8;">
-    //         <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Email</b></td>
-    //         <td style='padding: 10px; border: #e9e9e9 1px solid;'>${email}</td>
-    //       </tr>
-    //       <tr>
-    //         <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Message</b></td>
-    //         <td style='padding: 10px; border: #e9e9e9 1px solid;'>${message}</td>
-    //       </tr>
-    //     </table>
-    //   `,
-    // };
-    //
-    // console.log(process.env.ADMIN_EMAIL)
-    //
-    // await strapi.plugins['email'].services.email.sendTemplatedEmail(
-    //   {
-    //     to: process.env.ADMIN_EMAIL,
-    //     from: email,
-    //     replyTo: email,
-    //   },
-    //   emailTemplate,
-    // );
-
     ctx.send({message: 'ok'});
   },
 }));
